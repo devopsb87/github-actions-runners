b05b07283b390087650310bb148113877c71b4eb
Swetha-N169 <swethanandigama@gmail.com>
2026-05-18 22:46:17 +0530

    commit and push

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 git pull
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
 docker login
 docker build -t swethan567/github-runner:d87 .
 docker push swethan567/github-runner:d87
